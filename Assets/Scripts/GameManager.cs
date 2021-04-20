5c706f38000e6c6a3999111ac9fb913baefcd5f5
Benjamin <benjamin.daniel@student.hpi.de>
2021-04-20 20:29:30 +0200

    now working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,16 +18,17 @@ public class GameManager : MonoBehaviour
     public int trophyScore = 10000;
     public UIManager uiManager;
 
-    UpperHandle upperHandle;
-    LowerHandle lowerHandle;
-    SpeechIn speechIn;
-    SpeechOut speechOut;
-    int playerScore = 0;
-    int enemyScore = 0;
-    int gameScore = 0;
-    float totalTime = 0;
-    float levelStartTime = 0;
-    Dictionary<string, KeyCode> commands = new Dictionary<string, KeyCode>() {
+    private UpperHandle _upperHandle;
+    private LowerHandle _lowerHandle;
+    private SpeechIn _speechIn;
+    private SpeechOut _speechOut;
+    private int _playerScore = 0;
+    private int _enemyScore = 0;
+    private int _gameScore = 0;
+    private float _totalTime = 0;
+    private float _levelStartTime = 0;
+
+    private readonly Dictionary<string, KeyCode> _commands = new Dictionary<string, KeyCode>() {
         { "yes", KeyCode.Y },
         { "no", KeyCode.N },
         { "done", KeyCode.D }
@@ -35,8 +36,8 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        speechIn = new SpeechIn(onRecognized, commands.Keys.ToArray());
-        speechOut = new SpeechOut();
+        _speechIn = new SpeechIn(onRecognized, _commands.Keys.ToArray());
+        _speechOut = new SpeechOut();
 
         if (level < 0 || level >= enemyConfigs.Length)
         {
@@ -47,17 +48,17 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        upperHandle = GetComponent<UpperHandle>();
-        lowerHandle = GetComponent<LowerHandle>();
+        _upperHandle = GetComponent<UpperHandle>();
+        _lowerHandle = GetComponent<LowerHandle>();
 
-        uiManager.UpdateUI(playerScore, enemyScore);
+        uiManager.UpdateUI(_playerScore, _enemyScore);
 
         Introduction();
     }
 
     async void Introduction()
     {
-        await speechOut.Speak("Welcome to Quake Panto Edition");
+        await _speechOut.Speak("Welcome to Quake Panto Edition");
         // TODO: 1. Introduce obstacles in level 2 (aka 1)
         await Task.Delay(1000);
         RegisterColliders();
@@ -67,22 +68,22 @@ public class GameManager : MonoBehaviour
             await IntroduceLevel();
         }
 
-        await speechOut.Speak("Introduction finished, game starts.");
+        await _speechOut.Speak("Introduction finished, game starts.");
 
         await ResetGame();
     }
 
     async Task IntroduceLevel()
     {
-        await speechOut.Speak("There are two obstacles.");
+        await _speechOut.Speak("There are two obstacles.");
         Level level = GetComponent<Level>();
         await level.PlayIntroduction();
 
         // TODO: 2. Explain enemy and player with weapons by wiggling and playing shooting sound
 
-        await speechOut.Speak("Feel for yourself. Say yes or done when you're ready.");
+        await _speechOut.Speak("Feel for yourself. Say yes or done when you're ready.");
         //string response = await speechIn.Listen(commands);
-        await speechIn.Listen(new Dictionary<string, KeyCode>() { { "yes", KeyCode.Y }, { "done", KeyCode.D } });
+        await _speechIn.Listen(new Dictionary<string, KeyCode>() { { "yes", KeyCode.Y }, { "done", KeyCode.D } });
 
         //if (response == "yes")
         //{
@@ -95,8 +96,8 @@ public class GameManager : MonoBehaviour
     {
         while (true)
         {
-            await speechOut.Speak("Say done when you're ready.");
-            string response = await speechIn.Listen(commands);
+            await _speechOut.Speak("Say done when you're ready.");
+            string response = await _speechIn.Listen(_commands);
             if (response == "done")
             {
                 return;
@@ -120,23 +121,23 @@ public class GameManager : MonoBehaviour
     /// <returns></returns>
     async Task ResetGame()
     {
-        await speechOut.Speak("Spawning player");
+        await _speechOut.Speak("Spawning player");
         player.transform.position = playerSpawn.position;
-        await upperHandle.SwitchTo(player, 0.2f);
+        await _upperHandle.SwitchTo(player, 0.2f);
 
-        await speechOut.Speak("Spawning enemy");
+        await _speechOut.Speak("Spawning enemy");
         enemy.transform.position = enemySpawn.position;
         enemy.transform.rotation = enemySpawn.rotation;
-        await lowerHandle.SwitchTo(enemy, 0.2f);
+        await _lowerHandle.SwitchTo(enemy, 0.2f);
         if (level >= enemyConfigs.Length)
             Debug.LogError($"Level {level} is over number of enemies {enemyConfigs.Length}");
         enemy.GetComponent<EnemyLogic>().config = enemyConfigs[level];
 
-        upperHandle.Free();
+        _upperHandle.Free();
 
         player.SetActive(true);
         enemy.SetActive(true);
-        levelStartTime = Time.time;
+        _levelStartTime = Time.time;
     }
 
     async void onRecognized(string message)
@@ -146,8 +147,8 @@ public class GameManager : MonoBehaviour
 
     public void OnApplicationQuit()
     {
-        speechOut.Stop(); //Windows: do not remove this line.
-        speechIn.StopListening(); // [macOS] do not delete this line!
+        _speechOut.Stop(); //Windows: do not remove this line.
+        _speechIn.StopListening(); // [macOS] do not delete this line!
     }
 
     /// <summary>
@@ -164,18 +165,18 @@ public class GameManager : MonoBehaviour
 
         if (playerDefeated)
         {
-            enemyScore++;
+            _enemyScore++;
         }
         else
         {
-            playerScore++;
+            _playerScore++;
         }
-        uiManager.UpdateUI(playerScore, enemyScore);
+        uiManager.UpdateUI(_playerScore, _enemyScore);
 
         string defeatedPerson = playerDefeated ? "You" : "Enemy";
-        await speechOut.Speak($"{defeatedPerson} got defeated.");
+        await _speechOut.Speak($"{defeatedPerson} got defeated.");
 
-        gameScore += CalculateGameScore(player, enemy);
+        _gameScore += CalculateGameScore(player, enemy);
 
         level++;
         if (level >= enemyConfigs.Length)
@@ -184,8 +185,8 @@ public class GameManager : MonoBehaviour
         } else
         {
             // TODO: Evaluate the players performance with game score
-            await speechOut.Speak($"Current score is {gameScore}");
-            await speechOut.Speak($"Continuing with level {level + 1}");
+            await _speechOut.Speak($"Current score is {_gameScore}");
+            await _speechOut.Speak($"Continuing with level {level + 1}");
             await ResetGame();
         }
     }
@@ -196,21 +197,21 @@ public class GameManager : MonoBehaviour
     /// <returns></returns>
     async Task GameOver()
     {
-        await speechOut.Speak("Congratulations.");
+        await _speechOut.Speak("Congratulations.");
 
         if (!GetComponent<DualPantoSync>().debug)
         {
-            await speechOut.Speak($"You achieved a score of {gameScore}.");
-            await speechOut.Speak("Please enter your name to submit your highscore.");
+            await _speechOut.Speak($"You achieved a score of {_gameScore}.");
+            await _speechOut.Speak("Please enter your name to submit your highscore.");
 
-            await uiManager.GameOver(gameScore, (int)totalTime, trophyScore);
+            await uiManager.GameOver(_gameScore, (int)_totalTime, trophyScore);
         } else
         {
-            await speechOut.Speak($"You achieved a score of {gameScore} in debug mode.");
+            await _speechOut.Speak($"You achieved a score of {_gameScore} in debug mode.");
         }
 
-        await speechOut.Speak("Thanks for playing DuelPanto. Say quit when you're done.");
-        await speechIn.Listen(new Dictionary<string, KeyCode>() { { "quit", KeyCode.Escape } });
+        await _speechOut.Speak("Thanks for playing DuelPanto. Say quit when you're done.");
+        await _speechIn.Listen(new Dictionary<string, KeyCode>() { { "quit", KeyCode.Escape } });
 
         Application.Quit();
     }
@@ -227,8 +228,8 @@ public class GameManager : MonoBehaviour
         Health playerHealth = player.GetComponent<Health>();
         Health enemyHealth = enemy.GetComponent<Health>();
 
-        float levelCompleteTime = Time.time - levelStartTime;
-        totalTime += levelCompleteTime;
+        float levelCompleteTime = Time.time - _levelStartTime;
+        _totalTime += levelCompleteTime;
         int timeMultiplier = 1;
         if (levelCompleteTime < 30)
         {
